cbdc138dc1a3e22369f5042b4d78addcba2fea19
rootfastcell <ankitjha7022@gmail.com>
2020-06-07 14:13:48 +0530

    admin model

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -3,9 +3,6 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-
-
-
     def test_create_user_with_email_successfull(self):
         email = 'test@gmail.com'
         password= "test"
