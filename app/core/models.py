d4846883716807776947db3d1676dec64fa85a77
rootfastcell <ankitjha7022@gmail.com>
2020-06-08 08:04:05 +0530

    admin mode

--- app/core/models.py
+++ app/core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, \
                                        PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
         user =self.model(email=self.normalize_email(email), **extra_fields)
@@ -11,10 +11,11 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
 
         return user
-    def create_superuser(self,email,password):
-        user=self.create_user(email,password)
-        user.is_staff= True
-        user.is_superuser= True
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
